ce76a40b01daa926eabc9ef75fdd402a9d3afb9a
AlaaSlalaheldin <alaalolo2266@gmail.com>
2026-08-28 18:46:50 +0300

    task1

--- Advanced JS/script.js
+++ Advanced JS/script.js
@@ -1,7 +1,8 @@
-window.addEventListener("Load", function(){
+window.addEventListener("load", function(){
     var div = document.getElementsByClassName("test")[0]
     div.addEventListener("mousemove",function(e){
-        // div.innerText = 
+        div.innerText = "x: " + e.clientX + "y: " + e.clientY;
+        div.style.backgroundColor = `rgb(${e.clientX}) , ${e.clientY} , 100 `
     })
 
 })
